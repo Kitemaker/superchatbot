cc8b99966ed99e576e2f6a18de157cd66b15c2dc
Saleem Javed <Kitemaker@users.noreply.github.com>
2019-06-11 20:14:15 +0530

    latest

--- src/App.js
+++ src/App.js
@@ -39,8 +39,8 @@ class App extends Component {
             theme={myTheme}            
             botName="TableTrick"
             welcomeMessage="Welcome, how can I help you today?"
-            accessKeyId = {process.env.AWS_ACCESS_KEY_ID}
-            secretAccessKey ={process.env.AWS_SECRET_ACCESS_KEY}
+            accessKeyId = {process.env.accessKeyId}
+            secretAccessKey ={process.env.secretAccessKey}
             region ="us-east-1"    
           />        
 
